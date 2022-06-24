10abdedf493c528cb01d6113c1d7bc510609daab
Mauricio Viegas <mvrdgs@gmail.com>
2022-06-24 16:29:35 -0300

    feat: CreateSeller controller

--- internal/app/sellers/http/controller/handler.go
+++ internal/app/sellers/http/controller/handler.go
@@ -47,3 +47,36 @@ func (s *SellerHandler) GetOneSeller() gin.HandlerFunc {
 		ctx.JSON(code, web.NewResponse(code, seller, ""))
 	}
 }
+
+func (s *SellerHandler) CreateSeller() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req request
+		err := ctx.ShouldBindJSON(&req)
+		if err != nil {
+			ctx.JSON(http.StatusUnprocessableEntity, web.NewResponse(
+				http.StatusUnprocessableEntity, nil, "all fields must be correctly filled",
+				return
+			))
+		}
+
+		id, err := uuid.Parse(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "invalid id"))
+			return
+		}
+
+		cid, err := uuid.Parse(req.CID)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "invalid cid"))
+			return
+		}
+
+		seller, code, err := s.sellerService.CreateSeller(ctx, id, cid, req.CompanyName, req.Address, req.Telephone)
+		if err != nil {
+			ctx.JSON(code, web.NewResponse(code, nil, err.Error()))
+			return
+		}
+
+		ctx.JSON(code, web.NewResponse(code, seller, ""))
+	}
+}
