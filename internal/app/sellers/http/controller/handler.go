6a18c574c7ca93cb4a733027d43143a7a9712189
Mauricio Viegas <mvrdgs@gmail.com>
2022-06-24 16:29:05 -0300

    feat: GetOneSeller controller

--- internal/app/sellers/http/controller/handler.go
+++ internal/app/sellers/http/controller/handler.go
@@ -2,8 +2,10 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/mvrdgs/go-web-server/internal/app/sellers/domain"
 	"github.com/mvrdgs/go-web-server/pkg/web"
+	"net/http"
 )
 
 type SellerHandler struct {
@@ -25,5 +27,23 @@ func (s *SellerHandler) GetAllSellers() gin.HandlerFunc {
 		}
 
 		ctx.JSON(code, web.NewResponse(code, sellers, ""))
+	}
+}
+
+func (s *SellerHandler) GetOneSeller() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, err := uuid.Parse(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "invalid id"))
+			return
+		}
+
+		seller, code, err := s.sellerService.GetOneSeller(ctx, id)
+		if err != nil {
+			ctx.JSON(code, web.NewResponse(code, nil, err.Error()))
+			return
+		}
+
+		ctx.JSON(code, web.NewResponse(code, seller, ""))
 	}
 }
